382e2d7f045ce6c34c2f0ce40769cb0633d1f60d
Hamzailer <hamzailer>
2021-01-16 12:26:55 +0500

    replaced cover image

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -13,7 +13,7 @@ function Projects() {
       <Row className="mx-auto">
         <CustomCard
           title="Crystale"
-          image="https://pbs.twimg.com/profile_banners/1330400040314167297/1608641425/1500x500"
+          image="https://i.imgur.com/TPdGKD2.png"
           content="A Game Revolving Around the player trying to collect the Crystals of Authority in order to defeat the Monster Overlord."
           link="https://gamejolt.com/games/Crystale/525941"
         />
