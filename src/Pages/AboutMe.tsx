a7d15db337ae6891b875e92cb07132185106a97f
Hamzailer <hamzailer>
2021-01-16 15:16:41 +0500

    Updated Home Page and About Me Page

--- src/Pages/AboutMe.tsx
+++ src/Pages/AboutMe.tsx
@@ -3,10 +3,34 @@ import NavBar from "../Components/Navbar";
 
 function AboutMe() {
   return (
-    <div>
+    <div className="text-center">
       <NavBar />
       <br />
-      <h2 className="text-center">About Me</h2>
+      <h2>About Me</h2>
+      <br />
+      <p>
+        Hello There! I'm Hamzailer and I'm pretty new to the dev scene as before
+        <br />
+        this I was doing Game Dev (And still am) in the Godot Engine.
+      </p>
+
+      <br />
+      <p>
+        But I also wanted to try out making websites too as it seemed like fun
+        <br />
+        and turns out I was right it's been great using React and
+        Typescript/Javascript so far.
+        <br />
+        Although it was very confusing at first.
+        <br />
+        After reading some documentation and doing the tutorial at
+        <a href="htttps://reactjs.org/tutorial/tutorial.html"> ReactJS.org</a>
+        <br />
+        It was pretty easy to use and get this site up and running.
+        <br />
+        If you have anymore questions feel free to contact me at
+        Hamzailer@protonmail.com
+      </p>
     </div>
   );
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,7 +6,19 @@ function Home() {
     <div>
       <NavBar />
       <br />
-      <h2 className="text-center">Home</h2>
+      <h2 className="text-center">Welcome!</h2>
+      <br />
+      <p className="text-center">
+        My name's Hamzailer and I'm a solo indie dev and a rookie Web Developer.
+        <br />
+        <br />
+        If you wanna know more about me click on the About Me button on the
+        Navbar.
+        <br />
+        <br />
+        If you wanna checkout my projects click on the Projects button on the
+        Navbar
+      </p>
     </div>
   );
 }
